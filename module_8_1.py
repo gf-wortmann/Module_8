6cfbf6a0d577bf0e68f7f2ffe13a61f8ba321d80
George Wortmann <gf.wortmann@gmail.com>
2024-08-24 20:05:17 +0300

    1st commit

---
+++ module_8_1.py
@@ -0,0 +1,15 @@
+# Домашнее задание по теме "Try и Except".
+
+
+def add_everything_up(a, b):
+    try:
+        c = a + b
+    except TypeError:
+        return str(a) + str(b)
+    else:
+        return c
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
